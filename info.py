7125758fad3054be53a0d931e24d749bbcd20f9c
threatthreat <grezy8007@gmail.com>
2025-07-22 10:11:31 -0700

    Update info.py

--- info.py
+++ info.py
@@ -18,7 +18,7 @@ def is_enabled(value, default):
 SESSION = environ.get("SESSION", "Media_search")
 API_ID = int(environ.get("API_ID", "24222039"))
 API_HASH = environ.get("API_HASH", "6dd2dc70434b2f577f76a2e993135662")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7825091253:AAHfEWroAWrERPWY22goO3VKMyYSKjwAziI")
 PORT = environ.get("PORT", "8082")
 
 # Owners
